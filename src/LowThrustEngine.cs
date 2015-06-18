3286371ed8dea3b24c1131e6995fa9c7ab230bdb
Ben Diedrich <bldiedrich@gmail.com>
2015-06-18 12:27:19 -0700

    Use engine's enable/disable. Initialize name of propellant and density. Deplete propellant mass in timewarp. Retain throttle when going from timewarp to realtime.

--- src/LowThrustEngine.cs
+++ src/LowThrustEngine.cs
@@ -6,10 +6,6 @@ namespace PersistentThrust {
 
     public class LowThrustEngine : ModuleEngines {
 
-	// GUI to turn on persistent acceleration
-	[KSPField(isPersistant = true)]
-	public bool IsEnabled = false;
-
 	// GUI display values
 	// Thrust
 	[KSPField(guiActive = true, guiName = "Thrust")]
@@ -25,31 +21,34 @@ namespace PersistentThrust {
 	protected double thrust_d = 0;
 	protected double isp_d = 0;
 	protected double throttle_d = 0;
+	
+	// Persistent values to use during timewarp
+	float IspPersistent = 0;
+	float ThrustPersistent = 0;
+	float ThrottlePersistent = 0;
 
-	// GUI to activate persistent thrust
-	[KSPEvent(guiActive = true, guiName = "Activate Persistent Thrust", active = true)]
-	public void ActivatePersistentThrust() {
-	    IsEnabled = true;
-	}
+	// Are we transitioning from timewarp to reatime?
+	bool warpToReal = false;
 
-	// GUI to activate persistent thrust
-	[KSPEvent(guiActive = true, guiName = "Deactivate Persistent Thrust", active = false)]
-	public void DeactivatePersistentThrust() {
-	    IsEnabled = false;
-	}
-
+	// Resource used for deltaV and mass calculations
+	[KSPField]
+	public string resourceDeltaV;
+	// Density of resource
+	double density;
+	
 	// Update
 	public override void OnUpdate() {
 
-	    // Base class update
-	    base.OnUpdate();
-
+	    // When transitioning from timewarp to real update throttle
+	    if (warpToReal) {
+		vessel.ctrlState.mainThrottle = ThrottlePersistent;
+		warpToReal = false;
+	    }
+	    
 	    // Persistent thrust GUI
-	    Events["ActivatePersistentThrust"].active = !IsEnabled;
-	    Events["DeactivatePersistentThrust"].active = IsEnabled;
-	    Fields["Thrust"].guiActive = IsEnabled;
-	    Fields["Isp"].guiActive = IsEnabled;
-	    Fields["Throttle"].guiActive = IsEnabled;
+	    Fields["Thrust"].guiActive = isEnabled;
+	    Fields["Isp"].guiActive = isEnabled;
+	    Fields["Throttle"].guiActive = isEnabled;
 
 	    // Update display values
 	    Thrust = Utils.FormatThrust(thrust_d);
@@ -57,56 +56,52 @@ namespace PersistentThrust {
 	    Throttle = Math.Round(throttle_d * 100).ToString() + "%";
 	}
 
-	// Persistent values to use during timewarp
-	float IspPersistent = 0;
-	float ThrustPersistent = 0;
-	float ThrottlePersistent = 0;
+	// Initialization
+	public override void OnLoad(ConfigNode node) {
 
-	// Low thrust acceleration
-	public static Vector3d CalculateLowThrustForce (LowThrustEngine engine, float thrust, Vector3d up) {
-	    if (engine.part != null) {
-		return up * thrust;
-	    } else {
-		return Vector3d.zero;
-	    }
+	    // Run base OnLoad method
+	    base.OnLoad(node);
+
+	    // Initialize density of propellant used in deltaV and mass calculations
+	    density = PartResourceLibrary.Instance.GetDefinition(resourceDeltaV).density;
 	}
-
-	// Calculate deltaV
-	public static double CalculateDeltaV (float Isp, float m0, float thrust, double dT) {
-	    // Mass flow rate
-	    double mdot = thrust / (Isp * 9.81);
-	    // Final mass
-	    double m1 = m0 - mdot * dT;
-	    // DeltaV
-	    return Isp * 9.81 * Math.Log(m0 / m1);
-	}
-
+	
 	// Physics update
 	public override void OnFixedUpdate() {
 
-	    base.OnFixedUpdate();
-	    
-	    if (FlightGlobals.fetch != null && IsEnabled) {
-
+	    if (FlightGlobals.fetch != null && isEnabled) {
+		// Realtime mode
 		if (!this.vessel.packed) {
-		    // During realtime mode, update values to use during timewarp
-		    IspPersistent = realIsp;
-		    ThrottlePersistent = requestedThrottle;
-		    ThrustPersistent = this.CalculateThrust();
-		} else {
-		    // If in timewarp, perturb orbit using thrust
+		    // if not transitioning from warp to real
+		    // Update values to use during timewarp
+		    if (!warpToReal) {
+			IspPersistent = realIsp;
+			ThrottlePersistent = vessel.ctrlState.mainThrottle;
+			ThrustPersistent = this.CalculateThrust();
+		    }
+		} else { // Timewarp mode: perturb orbit using thrust
+		    warpToReal = true; // Set to true for transition to realtime
 		    double UT = Planetarium.GetUniversalTime(); // Universal time
 		    double dT = TimeWarp.fixedDeltaTime; // Time step size
 		    double m0 = this.vessel.GetTotalMass(); // Current mass
 		    double mdot = ThrustPersistent / (IspPersistent * 9.81); // Mass burn rate of engine
 		    double dm = mdot * dT; // Change in mass over dT
+		    double demand = dm / density; // Resource demand
+		    // Update vessel resource
+		    double demandOut = part.RequestResource(resourceDeltaV, demand);
+		    // Calculate thrust and deltaV if demand output > 0
 		    // TODO test if dm exceeds remaining propellant mass
-		    // TODO reduce propellant mass by dm
-		    double m1 = m0 - dm; // Mass at end of burn
-		    double deltaV = IspPersistent * 9.81 * Math.Log(m0/m1); // Delta V from burn
-		    Vector3d down = -this.part.transform.up; // Thrust direction
-		    Vector3d deltaVV = deltaV * down; // DeltaV vector
-		    vessel.orbit.Perturb(deltaVV, UT, dT); // Update vessel orbit
+		    if (demandOut > 0) {
+			double m1 = m0 - dm; // Mass at end of burn
+			double deltaV = IspPersistent * 9.81 * Math.Log(m0/m1); // Delta V from burn
+			Vector3d thrustV = this.part.transform.up; // Thrust direction
+			Vector3d deltaVV = deltaV * thrustV; // DeltaV vector
+			vessel.orbit.Perturb(deltaVV, UT, dT); // Update vessel orbit
+		    }
+		    // Otherwise, if throttle is turned on, and demand out is 0, show warning
+		    else if (ThrottlePersistent > 0) {
+			Debug.Log("Propellant depleted");
+		    }
 		}
 
 		// Update display numbers
